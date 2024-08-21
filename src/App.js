083b3eed7063cc51eae3e251189b382af0fed2ab
sandraluong <elieluong1993@gmail.com>
2024-08-21 15:06:24 +0200

    Add Search Engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./shecode logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="conatainer">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Project from Shecodes by Sandra</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
